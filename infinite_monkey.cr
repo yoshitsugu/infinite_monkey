aa8f6519c8b78515d404cba6c359cbaae497c903
Kota Yoshitsugu <k.yoshitsugu@mmj.ne.jp>
2015-08-07 09:54:28 +0900

    文節の数ずつしかチェックしてなかった

--- infinite_monkey.cr
+++ infinite_monkey.cr
@@ -5,28 +5,32 @@ class InfiniteMonkey
 
   def exec
     i = 1
-    loop do 
-      generated_string = generate
-      print generated_string
-      print ' '
-      break if correct_string == generated_string
+    strs = %w()
+    loop do
+      generated = generate
+      print generated
+      strs = swap(strs, generated)
+      break if correct_string == strs.join("")
       i += 1
     end
+    puts
     puts
     puts "#{i}回試行しました"
   end
 
   def generate : String
-    result = ""
-    @words.length.times do 
-      result += @words.sample
-    end
-    result
+    @words.sample
   end
 
   def correct_string : String
-   @words.join("")
+    @words.join("")
   end
+
+  def swap(strs : Array(String), str : String) : Array(String)
+    rotated = strs.dup
+    rotated.shift if rotated.length > (@words.length - 1)
+    rotated.push(str)
+  end   
 end
 
 InfiniteMonkey.new(%w(進捗 どう です か)).exec
